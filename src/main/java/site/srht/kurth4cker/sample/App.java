9fd3d96d74170bbc5519a5bd2b8fa1a103758ece
kurth4cker <kurth4cker@gmail.com>
2024-11-11 12:11:58 +0300

    sample.App: rename from Main

--- src/main/java/site/srht/kurth4cker/sample/App.java
+++ src/main/java/site/srht/kurth4cker/sample/App.java
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-package site.srht.kurth4cker;
+package site.srht.kurth4cker.sample;
 
-public final class Main {
+public final class App {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
