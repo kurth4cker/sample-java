a66f9e66d19badfb52d47488bcdd67e4761cd421
kurth4cker <kurth4cker@gmail.com>
2024-11-19 20:28:47 +0300

    try functional interfaces with IntStream

--- src/main/java/site/srht/kurth4cker/App.java
+++ src/main/java/site/srht/kurth4cker/App.java
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-package site.srht.kurth4cker.sample;
+package site.srht.kurth4cker;
+
+import java.util.stream.IntStream;
+import site.srht.kurth4cker.NumberUtil;
 
 public final class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        IntStream.range(0, 100)
+            .filter(NumberUtil::isPrime)
+            .forEach(System.out::println);
+        // for (int i = 0; i < 100; i++) {
+        //     if (NumberUtil.isPrime(i)) {
+        //         System.out.println(i);
+        //     }
+        // }
     }
 }
