3eb26fa36f5191411e771a7bc233f171433fbeb3
kurth4cker <kurth4cker@gmail.com>
2024-12-22 12:05:20 +0300

    unstable.*: remove lots of class
    They are moved to java-sample[1].
    
    [1]: https://git.sr.ht/~kurth4cker/java-sample

--- src/main/java/site/srht/kurth4cker/unstable/FIFO.java
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
-
-package site.srht.kurth4cker.unstable;
-
-public interface FIFO<E> {
-    void add(E e);
-    E remove();
-}
--- src/main/java/site/srht/kurth4cker/unstable/LIFO.java
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
-
-package site.srht.kurth4cker.unstable;
-
-public interface LIFO<E> {
-    void push(E e);
-    E pop();
-}
